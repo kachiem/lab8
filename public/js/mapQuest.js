f1de011030a182f7368bbcf8d87426ff2e1331d9
kachiem <kachiem@gmail.com>
2019-03-01 09:24:00 -0800

    mapquest

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'muEPjE6FpK99TYTnhGvuYgU4u3PYbFAE';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+		center: [32.88, -117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	// add marker to map
+	L.marker([32.88, -117.236]).addTo(map);
 }
